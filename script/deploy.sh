fdf93373f79ef5d1ad171a5792563f3f59eba1a8
unknown <imadeddine.djelaili@ifsalpha.com>
2019-04-27 16:38:49 +0200

    deplyement stack

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 REGION="eu-west-1"
 aws cloudformation deploy \
-  --template-file cloudformation/front.yml \
-  --stack-name staging-mooddy-app \
+  --template-file cloudformation/front_cloudfront.yml \
+  --stack-name staging-mooddy-app-tow \
+  --parameter-overrides StaticHosting=app.mooddy.net Domain=app \
   --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
   --region ${REGION} \
   --no-fail-on-empty-changeset
