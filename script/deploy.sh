72b5f0a4d9d8af37817bed54fd516abdd1e39b7a
idjelaili <imadeddinedjelaili@gmail.com>
2020-09-20 20:14:52 +0200

    new version

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,9 @@
 REGION="eu-west-1"
 aws cloudformation deploy \
   --template-file cloudformation/front_cloudfront.yml \
-  --stack-name staging-mooddy-app-tow \
-  --parameter-overrides StaticHosting=app.mooddy.net Domain=app \
+  --stack-name staging-mooddy-app-com \
+  --parameter-overrides StaticHosting=www.mooddy-app.com Domain=www \
   --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
   --region ${REGION} \
   --no-fail-on-empty-changeset
-aws s3 cp src/ s3://app.mooddy.net/ --recursive
+aws s3 cp src/ s3://www.mooddy-app.com-artifact/ --recursive
